7daf86891a902891e1663994f62bff5489eb86c8
Shakeb Ameen <shakebameen@gmail.com>
2019-05-29 11:30:26 +0800

    added lists and array adapters

--- app/src/main/java/com/shakebameen/beaconizer/MainActivity.java
+++ app/src/main/java/com/shakebameen/beaconizer/MainActivity.java
@@ -2,10 +2,14 @@ package com.shakebameen.beaconizer;
 
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.cardview.widget.CardView;
+import androidx.annotation.Nullable;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -18,6 +22,7 @@ import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -44,7 +49,10 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
     private final String TAG = "Beaconizer";
     private BeaconManager mBeaconManager;
     private float BeaconDistance;
-    CardView cardView = cardView = findViewById(R.id.cardView);
+    private ListView listView ;
+    private ArrayList<String> stringArrayList = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter ;
+
 
     //Firebase Reference
     FirebaseStorage FirebaseStorageRef = FirebaseStorage.getInstance();
@@ -56,7 +64,14 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
     public StorageReference Nike   = imageStorageRef.child("Nike01.jpg");
     public StorageReference Puma = imageStorageRef.child("Puma01.jpg");
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, stringArrayList);
+        listView = (ListView) findViewById(R.id.lstView);
 
+    }
 
     @Override
     public void onResume() {
@@ -69,9 +84,12 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
         mBeaconManager.bind(this);
+
+
     }
 
     public void onBeaconServiceConnect() {
+
         Region region = new Region("all-beacons-region", null, null, null);
         try {
             mBeaconManager.startRangingBeaconsInRegion(region);
@@ -79,13 +97,16 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
             e.printStackTrace();
         }
         mBeaconManager.addRangeNotifier(this);
+
     }
 
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
         for (Beacon beacon: beacons) {
             if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+
                 // This is a Eddystone-UID frame
+
                 Identifier namespaceId = beacon.getId1();
                 Identifier instanceId = beacon.getId2();
                 BeaconDistance = (float) beacon.getDistance();
@@ -93,9 +114,15 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
                         " and instance id: "+instanceId+
                         " approximately "+BeaconDistance+" meters away.");
 
-                
+
+                //adding beacon to the array
+                stringArrayList.add("UUID - "+namespaceId+instanceId);
+                arrayAdapter.notifyDataSetChanged();
+
+                //Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
 
                 // Do we have telemetry data?
+
                 if (beacon.getExtraDataFields().size() > 0) {
                     long telemetryVersion = beacon.getExtraDataFields().get(0);
                     long batteryMilliVolts = beacon.getExtraDataFields().get(1);
@@ -103,7 +130,8 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
                     long uptime = beacon.getExtraDataFields().get(4);
 
                     Log.d(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
-                            ", has been up for : "+uptime+" seconds"+
+                            ", has " +
+                            "been up for : "+uptime+" seconds"+
                             ", has a battery level of "+batteryMilliVolts+" mV"+
                             ", and has transmitted "+pduCount+" advertisements.");
 
