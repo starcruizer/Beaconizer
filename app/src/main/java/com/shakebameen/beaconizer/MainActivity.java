a0587ab0742a1cee22203e3ea8303e1398d4e21a
Stephen <stephenmaish218@gmail.com>
2019-06-06 13:04:07 +0800

    removed un necessary commets

--- app/src/main/java/com/shakebameen/beaconizer/MainActivity.java
+++ app/src/main/java/com/shakebameen/beaconizer/MainActivity.java
@@ -25,25 +25,6 @@ import org.altbeacon.beacon.Region;
 import java.util.ArrayList;
 import java.util.Collection;
 
-/**
- * Skeleton of an Android Things activity.
- * <p>
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * mLedGpio.setValue(true);
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- */
 public class MainActivity extends Activity implements BeaconConsumer, RangeNotifier {
 
     private final String TAG = "Beaconizer";
